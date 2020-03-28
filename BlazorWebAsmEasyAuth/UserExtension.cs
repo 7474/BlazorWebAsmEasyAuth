884de9cf9a02845d48ba9ee5d85d3ab3c1a93c7e
koudenpa <koudenpa@hotmail.com>
2020-03-29 01:44:53 +0900

    ClaimsPrincipal の拡張メソッドを追加。

---
+++ BlazorWebAsmEasyAuth/UserExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace BlazorWebAsmEasyAuth
+{
+    public static class UserExtension
+    {
+        public static string AvatarIconUrl(this ClaimsPrincipal user)
+        {
+            // TODO 他のプロバイダにも対応する。
+            return user.FindFirst("urn:twitter:profile_image_url_https")?.Value;
+        }
+        public static bool HasAvatarIcon(this ClaimsPrincipal user)
+        {
+            return !string.IsNullOrEmpty(AvatarIconUrl(user));
+        }
+    }
+}
